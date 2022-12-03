e5e0e8f0447b73a593fbe007424ed9ccdb6a7e0a
paolo.carraro <paolo.carraro@interlogica.it>
2022-12-03 02:42:14 +0100

    Update ModalDialog with new props (level, buttons, ecc)

--- app/basic-elements/page.tsx
+++ app/basic-elements/page.tsx
@@ -2,24 +2,43 @@
 
 import { useState } from "react";
 import Dropdown from "../../components/elements/Dropdown";
-import ModalDialog from "../../components/elements/ModalDialog";
+import ModalDialog, { ModalDialogButtonProps } from "../../components/elements/ModalDialog";
+
+
+
 
 export default function BasicElements() {
 
-    // const [modalIsOpen, setModalIsOpen] = useState(false);
+    const [modalIsOpen, setModalIsOpen] = useState(false);
+
+    const buttonsProps: ModalDialogButtonProps[] = [
+        { label: "Ok", handler: () => setModalIsOpen(false), style: "primary" },
+        { label: "Cancel", handler: () => setModalIsOpen(false) },
+    ];
 
     return (
-        <section>
-            <h1>Basic elements</h1>
-            <h2>Basic elements h2</h2>
-            <div >
-                <Dropdown />
-            </div>
-            <div >
-                <ModalDialog title="Attention this is a test" show={true}>
-                    <p>This is dialog body where user can read something about process and take a decision</p>
-                </ModalDialog>
-            </div>
-        </section>
+        <>
+            <section>
+                <h1>Basic elements</h1>
+                <div className="mb-4">
+                    <Dropdown />
+                </div>
+                <div className="mb-4">
+                    <button onClick={() => setModalIsOpen(true)}>Open modal dialog </button>
+                    <p>button in <strong>default</strong> style</p>
+                </div>
+
+                <div className="mb-4">
+                    <button className="primary" onClick={() => setModalIsOpen(true)}>Open modal dialog</button>
+                    <p>button in <strong>primary</strong> style</p>
+                </div>
+
+            </section>
+
+            <ModalDialog title="Information" show={modalIsOpen} onClose={() => setModalIsOpen(false)} buttons={buttonsProps} level="info">
+                <p>This is dialog body where user can read something to</p>
+            </ModalDialog>
+        </>
+
     )
 }
--- components/elements/ModalDialog.tsx
+++ components/elements/ModalDialog.tsx
@@ -1,11 +1,15 @@
-'use client'
 
 import { Fragment, ReactNode, useRef, useState } from 'react'
 import { Dialog, Transition } from '@headlessui/react'
-import { ExclamationTriangleIcon } from '@heroicons/react/24/outline'
+import { ExclamationTriangleIcon, CheckIcon } from '@heroicons/react/24/outline'
+import classNames from 'classnames';
+import { InformationCircleIcon } from '@heroicons/react/20/solid';
+
 
 export type ModalDialogButtonProps = {
     label: string;
+    focus?: boolean;
+    style?: string
     handler: () => void;
 }
 
@@ -13,17 +17,18 @@ export type ModalDialogProps = {
     title: string;
     children: ReactNode
     show: boolean;
-    buttons?: ModalDialogButtonProps[]
+    buttons?: ModalDialogButtonProps[];
+    level: "success" | "error" | "warning" | "info"
+    onClose: () => void;
 }
 
-export default function ModalDialog({ title, children, show }: ModalDialogProps) {
-    const [open, setOpen] = useState(show)
+export default function ModalDialog({ title, children, show, onClose, buttons, level }: ModalDialogProps) {
 
     const cancelButtonRef = useRef(null)
 
     return (
-        <Transition.Root show={open} as={Fragment}>
-            <Dialog as="div" className="relative z-10" initialFocus={cancelButtonRef} onClose={setOpen}>
+        <Transition.Root show={show} as={Fragment}>
+            <Dialog as="div" className="relative z-10" initialFocus={cancelButtonRef} onClose={onClose}>
                 <Transition.Child
                     as={Fragment}
                     enter="ease-out duration-300"
@@ -50,8 +55,16 @@ export default function ModalDialog({ title, children, show }: ModalDialogProps)
                             <Dialog.Panel className="relative transform overflow-hidden rounded-lg bg-white text-left shadow-xl transition-all sm:my-8 sm:w-full sm:max-w-lg">
                                 <div className="bg-white px-4 pt-5 pb-4 sm:p-6 sm:pb-4">
                                     <div className="sm:flex sm:items-start">
-                                        <div className="mx-auto flex h-12 w-12 flex-shrink-0 items-center justify-center rounded-full bg-red-100 sm:mx-0 sm:h-10 sm:w-10">
-                                            <ExclamationTriangleIcon className="h-6 w-6 text-red-600" aria-hidden="true" />
+                                        <div className={classNames(
+                                            "mx-auto flex h-12 w-12 flex-shrink-0 items-center justify-center rounded-full sm:mx-0 sm:h-10 sm:w-10",
+                                            { "bg-red-200": level === 'error' },
+                                            { "bg-green-200": level === 'success' },
+                                            { "bg-yellow-200": level === 'warning' },
+                                            { "bg-cyan-200": level === 'info' })}>
+                                            {level === 'success' ? <CheckIcon className="h-6 w-6 text-green-600" aria-hidden="true" /> : null}
+                                            {level === 'error' ? <ExclamationTriangleIcon className="h-6 w-6 text-red-600" aria-hidden="true" /> : null}
+                                            {level === 'warning' ? <ExclamationTriangleIcon className="h-6 w-6 text-yellow-600" aria-hidden="true" /> : null}
+                                            {level === 'info' ? <InformationCircleIcon className="h-6 w-6 text-cyan-600" aria-hidden="true" /> : null}
                                         </div>
                                         <div className="mt-3 text-center sm:mt-0 sm:ml-4 sm:text-left">
                                             <Dialog.Title as="h3" className="text-lg font-medium leading-6 text-gray-900">
@@ -63,22 +76,14 @@ export default function ModalDialog({ title, children, show }: ModalDialogProps)
                                         </div>
                                     </div>
                                 </div>
-                                <div className="bg-gray-50 px-4 py-3 sm:flex sm:flex-row-reverse sm:px-6">
-                                    <button
-                                        type="button"
-                                        className="inline-flex w-full justify-center rounded-md border border-transparent bg-red-600 px-4 py-2 text-base font-medium text-white shadow-sm hover:bg-red-700 focus:outline-none focus:ring-2 focus:ring-red-500 focus:ring-offset-2 sm:ml-3 sm:w-auto sm:text-sm"
-                                        onClick={() => setOpen(false)}
-                                    >
-                                        Deactivate
-                                    </button>
-                                    <button
-                                        type="button"
-                                        className="mt-3 inline-flex w-full justify-center rounded-md border border-gray-300 bg-white px-4 py-2 text-base font-medium text-gray-700 shadow-sm hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2 sm:mt-0 sm:ml-3 sm:w-auto sm:text-sm"
-                                        onClick={() => setOpen(false)}
-                                        ref={cancelButtonRef}
-                                    >
-                                        Cancel
-                                    </button>
+                                <div className="bg-gray-50 px-4 py-3 sm:flex sm:flex-row-reverse sm:px-6 gap-2">
+                                    {
+                                        buttons?.map((bp, i) => {
+                                            return (
+                                                <button key={`modal-button${i}`} type="button" className={bp.style} onClick={() => bp.handler()}>{bp.label}</button>
+                                            )
+                                        })
+                                    }
                                 </div>
                             </Dialog.Panel>
                         </Transition.Child>

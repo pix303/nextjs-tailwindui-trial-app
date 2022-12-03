3d3357abf5a48d1c19fe95883cdfea310ed552c2
paolo.carraro <paolo.carraro@interlogica.it>
2022-12-03 02:40:40 +0100

    Add typography page

---
+++ app/typography/page.tsx
@@ -0,0 +1,39 @@
+export default function Typography() {
+    return (
+        <section>
+            <h1>Title h1</h1>
+            <h2>Title h2</h2>
+            <h3>Title h3</h3>
+            <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Sit dolor voluptatibus maxime at officia praesentium ipsum, repellendus maiores soluta nemo ea expedita natus tenetur commodi excepturi corrupti vero odit? Blanditiis.</p>
+            <table>
+                <tr>
+                    <th>First Name</th>
+                    <th>Last Name</th>
+                    <th>Email Address</th>
+                </tr>
+                <tr>
+                    <td>Hillary</td>
+                    <td>Nyakundi</td>
+                    <td>tables@mail.com</td>
+                </tr>
+                <tr>
+                    <td>Lary</td>
+                    <td>Mak</td>
+                    <td>developer@mail.com</td>
+                </tr>
+            </table>
+            <h3>Unordered list</h3>
+            <ul>
+                <li>Coffee</li>
+                <li>Tea</li>
+                <li>Milk</li>
+            </ul>
+            <h3>Ordered list</h3>
+            <ol>
+                <li>Coffee</li>
+                <li>Tea</li>
+                <li>Milk</li>
+            </ol>
+        </section>
+    );
+} 
